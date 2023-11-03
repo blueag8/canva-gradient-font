dfa89f65fedf9283925e7803345928e9e619139b
Naomi Wickham <ns.wickham08@outlook.com>
2023-11-03 11:58:46 +0000

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+function GenerateFont() {
+  return (
+    <button>
+     Submit
+    </button>
+  );
+}
+
+export default function GradientFont() {
+  return (
+    <div>
+      <h1>Welcome to Beautifont!</h1>
+      <GenerateFont />
+    </div>
+  );
+}
